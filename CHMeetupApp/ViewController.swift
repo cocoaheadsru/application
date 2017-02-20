82663c9ff494990f71d2b35d0dd3eb5bfadc2032
Alexander Zimin <azimin@me.com>
2017-02-20 18:07:50 +0300

    added xcode project

---
+++ CHMeetupApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CHMeetupApp
+//
+//  Created by Alexander Zimin on 20/02/2017.
+//  Copyright © 2017 CocoaHeads Comunity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
