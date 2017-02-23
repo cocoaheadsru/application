a96584d2bb23c837dae436e3fc407b84b6128ffa
Egor Petrov <petrov.bz.molod@gmail.com>
2017-02-23 12:52:00 +0300

    [Vuq6BkBC] Add test file

---
+++ CHMeetupApp/Sources/Common/Classes/importToCalendar.swift
@@ -0,0 +1,9 @@
+//
+//  importToCalendar.swift
+//  CHMeetupApp
+//
+//  Created by Егор Петров on 23/02/2017.
+//  Copyright © 2017 CocoaHeads Comunity. All rights reserved.
+//
+
+import Foundation
