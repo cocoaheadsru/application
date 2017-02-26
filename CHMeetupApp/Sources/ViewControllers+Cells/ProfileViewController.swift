dc3d1ea166ba7e231db372a2bac2607cf861366a
Igor Tudoran <igortudoran@me.com>
2017-02-26 22:33:01 +0200

    [t9E0XoOt] made simple profile viewcontroller

--- CHMeetupApp/Sources/ViewControllers+Cells/ProfileViewController.swift
+++ CHMeetupApp/Sources/ViewControllers+Cells/ProfileViewController.swift
@@ -8,17 +8,92 @@
 
 import UIKit
 
+struct Cells {
+  struct Profile {
+    static let picture =  "ProfilePictureCell"
+    static let name =     "ProfileNameCell"
+    static let speach =   "ProfileSpeachCell"
+  }
+}
+
+struct CellHeights {
+  struct Profile {
+    static let picture =  CGFloat(104.0)
+    static let name =     CGFloat(63.0)
+    static let speach =   CGFloat(37.0)
+  }
+}
+
+enum ProfileCells {
+  case picture, name, speach
+}
+
 class ProfileViewController: UIViewController, ProfileHierarhyViewControllerType {
+
+  @IBOutlet weak var tableView: UITableView!
+
+  var tableArray = [ProfileCells]()
+
+  // MARK: - View Lifecycle.
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    // Do any additional setup after loading the view.
+    setupTableView()
+    updateTableView()
   }
+
+  // MARK: - Setup & updates.
+
+  func setupTableView() {
+    tableView.tableFooterView = UIView()
+    tableView.register(UINib.init(nibName: Cells.Profile.picture, bundle: nil),
+                       forCellReuseIdentifier: Cells.Profile.picture)
+    tableView.register(UINib.init(nibName: Cells.Profile.name, bundle: nil),
+                       forCellReuseIdentifier: Cells.Profile.name)
+    tableView.register(UINib.init(nibName: Cells.Profile.speach, bundle: nil),
+                       forCellReuseIdentifier: Cells.Profile.speach)
+  }
+
+  func updateTableView() {
+    tableArray = [.picture, .name, .speach]
+    tableView.reloadData()
+  }
+
+  // MARK: - Actions.
 
   @IBAction func logutBarButtonAction(_ sender: UIBarButtonItem) {
     LoginProcessViewController.isLogin = false
     profileNavigationController?.updateRootViewController()
+  }
+
+  @IBAction func editBarButtonAction(_ sender: UIBarButtonItem) {
+
+  }
+
+  // MARK: - Table helpers.
+
+  func cellHeightFor(_ indexPath: IndexPath) -> CGFloat {
+    switch tableArray[indexPath.row] {
+    case .picture:
+      return CellHeights.Profile.picture
+    case .name:
+      return CellHeights.Profile.name
+    case .speach:
+      return CellHeights.Profile.speach
+    }
+  }
+
+  func cellObjectFor(_ indexPath: IndexPath) -> UITableViewCell {
+    var cell: UITableViewCell!
+    switch tableArray[indexPath.row] {
+    case .picture:
+      cell = tableView.dequeueReusableCell(withIdentifier: Cells.Profile.picture, for: indexPath)
+    case .name:
+      cell = tableView.dequeueReusableCell(withIdentifier: Cells.Profile.name, for: indexPath)
+    case .speach:
+      cell = tableView.dequeueReusableCell(withIdentifier: Cells.Profile.speach, for: indexPath)
+    }
+    return cell
   }
 
 }
@@ -26,19 +101,23 @@ class ProfileViewController: UIViewController, ProfileHierarhyViewControllerType
 extension ProfileViewController: UITableViewDataSource {
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return tableArray.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return UITableViewCell()
+    return cellObjectFor(indexPath)
   }
 
 }
 
 extension ProfileViewController: UITableViewDelegate {
 
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return cellHeightFor(indexPath)
+  }
 
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    tableView.deselectRow(at: indexPath, animated: true)
   }
 
 }
