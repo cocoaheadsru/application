c784c467be90e73fa071e7bb09b528cd06e620e5
John Die <smejlum@gmail.com>
2017-02-28 09:29:20 +0300

    [8uM2FzET] Restructured remoteservice

--- CHMeetupApp/Sources/ViewControllers+Cells/TabBarViewController.swift
+++ CHMeetupApp/Sources/ViewControllers+Cells/TabBarViewController.swift
@@ -14,17 +14,22 @@ class TabBarViewController: UITabBarController {
     super.viewDidLoad()
 
     // Query example
-    do {
-      try remoteRequests.load(.users) { json in
-        let users = json.flatMap(UserPO.init)
-        for user in users {
-          print(user.name)
-          print(user.remoteID)
-        }
-      }
-    } catch {
-      print("Remote loading error: \(error) for resourse: \(API.users)")
-    }
+
+    Server.request<UserPO>(UserPO.Requests.list, completion: nil)
+    
+    
+//    
+//    do {
+//      try remoteRequests.load(.users) { json in
+//        let users = json.flatMap(UserPO.init)
+//        for user in users {
+//          print(user.name)
+//          print(user.remoteID)
+//        }
+//      }
+//    } catch {
+//      print("Remote loading error: \(error) for resourse: \(API.users)")
+//    }
 
   }
 
