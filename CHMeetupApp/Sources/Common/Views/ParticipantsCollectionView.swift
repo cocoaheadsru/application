3f4beaeacf7397469d523b90e2358448f0bdc1a8
DmitriyLis <expertgames@yandex.ru>
2017-03-17 15:33:44 +0300

    [DRUt94Cf] Add extension, rename custom view, code improvements

--- CHMeetupApp/Sources/Common/Views/ParticipantsCollectionView.swift
+++ CHMeetupApp/Sources/Common/Views/ParticipantsCollectionView.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ParticipantsCollection: UIView {
+class ParticipantsCollectionView: UIView {
 
   var imagesCollection: [UIImage] = [] {
     didSet {
@@ -40,12 +40,13 @@ class ParticipantsCollection: UIView {
       let viewHeight = self.bounds.height
       let viewWidth = self.bounds.width
 
-      let xImageView = (CGFloat(index) * viewHeight) * 0.8 // each new element takes 80% of view height
+      let xImageView = ((CGFloat(index) * viewHeight) * 0.8).round(0.5) // each new element takes 80% of view height
       let leadingEdgeSecondImageView = ((CGFloat(index + 1) * viewHeight) * 0.8) + viewHeight
 
       let imageView = UIImageView()
       imageView.clipsToBounds = true
-      imageView.layer.borderWidth = self.bounds.height * 0.05 // borderWidth = 5% of view height
+      imageView.contentMode = .scaleAspectFill
+      imageView.layer.borderWidth = viewHeight * 0.05 // borderWidth = 5% of view height
       imageView.layer.cornerRadius = viewHeight / 2 // cornerRadius = 50% of view height
       imageView.layer.borderColor = borderColor
 
