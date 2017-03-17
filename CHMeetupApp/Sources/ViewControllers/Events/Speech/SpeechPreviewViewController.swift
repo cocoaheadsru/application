6a6eac75bf70152e89835fe62cdb09ad006dc85a
GlobakMaxim <maxim.globak@gmail.com>
2017-03-18 01:44:04 +0300

    [e1Wh8FHb] Added debug table and small improvements

--- CHMeetupApp/Sources/ViewControllers/Events/Speech/SpeechPreviewViewController.swift
+++ CHMeetupApp/Sources/ViewControllers/Events/Speech/SpeechPreviewViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class SpeechPreviewViewController: UIViewController {
+class SpeechPreviewViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+  @IBOutlet weak var tableView: UITableView! {
+    didSet {
+      tableView.delegate = self
+      tableView.dataSource = self
+      tableView.registerNib(for: SpeakerTableViewCell.self)
+      tableView.backgroundColor = UIColor(red: 0.922, green: 0.929, blue: 0.929, alpha: 1.00)
+    }
+  }
   override func viewDidLoad() {
     super.viewDidLoad()
 
     title = "Speech".localized
+    view.backgroundColor = UIColor(red: 0.922, green: 0.929, blue: 0.929, alpha: 1.00)
   }
 
+  // MARK: - UITableViewDelegate
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return 260
+  }
+
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 1
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    return tableView.dequeueReusableCell(for: indexPath) as SpeakerTableViewCell
+  }
+
+  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print("debug tap")
+  }
 }
--- CHMeetupApp/Sources/ViewControllers/Events/Speech/subviews/SpeakerTableViewCell.swift
+++ CHMeetupApp/Sources/ViewControllers/Events/Speech/subviews/SpeakerTableViewCell.swift
@@ -9,38 +9,42 @@
 import UIKit
 
 class SpeakerTableViewCell: PlateTableViewCell {
-  @IBOutlet weak var backplateView: UIView! {
-    didSet {
-      backplateView.layer.cornerRadius = 4
-    }
-  }
   @IBOutlet weak var avatarImageView: UIImageView! {
     didSet {
       avatarImageView.layer.cornerRadius = avatarImageView.bounds.size.height/2
+      avatarImageView.backgroundColor = UIColor(.lightGray)
     }
   }
   @IBOutlet weak var fullNameLabel: UILabel! {
     didSet {
-      fullNameLabel.font = UIFont.appFont(.gothamPro(size: 16))
+      fullNameLabel.font = UIFont.appFont(.gothamProMedium(size: 16))
       fullNameLabel.textColor = UIColor(.black)
+      fullNameLabel.adjustsFontSizeToFitWidth = true
+      fullNameLabel.minimumScaleFactor = 0.5
     }
   }
   @IBOutlet weak var speakerDescriptionLabel: UILabel! {
     didSet {
       speakerDescriptionLabel.font = UIFont.appFont(.systemFont(size: 14))
       speakerDescriptionLabel.textColor = UIColor(.darkGray) // should be #666666
+      speakerDescriptionLabel.adjustsFontSizeToFitWidth = true
+      speakerDescriptionLabel.minimumScaleFactor = 0.5
     }
   }
   @IBOutlet weak var topicPreLabel: UILabel! {
     didSet {
       topicPreLabel.font = UIFont.appFont(.gothamPro(size: 15))
       topicPreLabel.textColor = UIColor(.black)
+      topicPreLabel.adjustsFontSizeToFitWidth = true
+      topicPreLabel.minimumScaleFactor = 0.5
     }
   }
   @IBOutlet weak var topicLabel: UILabel! {
     didSet {
       topicLabel.font = UIFont.appFont(.systemFont(size: 17))
       topicLabel.textColor = UIColor(.black)
+      topicLabel.adjustsFontSizeToFitWidth = true
+      topicLabel.minimumScaleFactor = 0.5
     }
   }
   @IBOutlet weak var seporatorLineView: UIView! {
@@ -52,6 +56,8 @@ class SpeakerTableViewCell: PlateTableViewCell {
     didSet {
       speachDescriptionLabel.font = UIFont.appFont(.systemFont(size: 15))
       speachDescriptionLabel.textColor = UIColor(.darkGray) // should be #666666
+      speachDescriptionLabel.adjustsFontSizeToFitWidth = true
+      speachDescriptionLabel.minimumScaleFactor = 0.5
     }
   }
 
