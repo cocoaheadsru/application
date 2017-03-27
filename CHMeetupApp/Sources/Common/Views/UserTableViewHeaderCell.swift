fe6847b5a64e64591aae008a77cd100fc8e63835
DmitriyLis <expertgames@yandex.ru>
2017-03-28 02:09:08 +0300

    [IHndnHqP] Improved after comments

--- CHMeetupApp/Sources/Common/Views/UserTableViewHeaderCell.swift
+++ CHMeetupApp/Sources/Common/Views/UserTableViewHeaderCell.swift
@@ -11,24 +11,24 @@ import UIKit
 class UserTableViewHeaderCell: UITableViewCell {
 
   @IBOutlet var positionAtCompanyLabel: UILabel!
-  @IBOutlet var userImageView: UIImageView! {
-    didSet {
-      userImageView.layer.cornerRadius = userImageView.bounds.height / 2 // cornerRadius = 50% of view height
+  @IBOutlet var userImageView: UIImageView!
 
-      let border = CAShapeLayer()
-      border.frame = userImageView.bounds
-      border.lineWidth = (userImageView.bounds.height * 0.08).round(0.5) // borderWidth = 4% of view height x2
-      border.path = UIBezierPath(ovalIn: border.bounds).cgPath
-      border.strokeColor = UIColor.white.cgColor
-      border.fillColor = UIColor.clear.cgColor
-      userImageView.layer.addSublayer(border)
-    }
+  override func layoutSubviews() {
+    userImageView.layer.cornerRadius = userImageView.bounds.height / 2 // cornerRadius = 50% of view height
+
+    let border = CAShapeLayer()
+    border.frame = userImageView.bounds
+    border.lineWidth = (userImageView.bounds.height * 0.08).round(0.5) // borderWidth = 4% of view height x2
+    border.path = UIBezierPath(ovalIn: border.bounds).cgPath
+    border.strokeColor = UIColor.white.cgColor
+    border.fillColor = UIColor.clear.cgColor
+    userImageView.layer.addSublayer(border)
   }
 
   override func awakeFromNib() {
     super.awakeFromNib()
 
     contentView.backgroundColor = UIColor(.lightGray)
-    self.selectionStyle = .none
+    selectionStyle = .none
   }
 }
