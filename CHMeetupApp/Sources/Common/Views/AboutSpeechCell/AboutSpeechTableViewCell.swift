2c904bad12234eca4575325527c201ca0e54c655
Kirill Averyanov <Kirillzzy@gmail.com>
2017-03-30 01:44:02 +0300

    [ivyPvV8d] Cell with fields

---
+++ CHMeetupApp/Sources/Common/Views/AboutSpeechCell/AboutSpeechTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  AboutSpeechTableViewCell.swift
+//  CHMeetupApp
+//
+//  Created by Kirill Averyanov on 29/03/2017.
+//  Copyright © 2017 CocoaHeads Community. All rights reserved.
+//
+
+import UIKit
+
+class AboutSpeechTableViewCell: PlateTableViewCell {
+
+  @IBOutlet var infoLabel: UILabel! {
+    didSet {
+      infoLabel.font = UIFont.appFont(.gothamProMedium(size: 15))
+      infoLabel.textColor = .lightGray
+    }
+  }
+  @IBOutlet var titleLabel: UILabel! {
+    didSet {
+      titleLabel.font = UIFont.appFont(.gothamProMedium(size: 17))
+      titleLabel.textColor = .black
+    }
+  }
+  @IBOutlet var descriptionLabel: UILabel! {
+    didSet {
+      descriptionLabel.font = UIFont.appFont(.gothamPro(size: 17))
+      descriptionLabel.textColor = .black
+    }
+  }
+
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    roundType = .all
+  }
+
+}
