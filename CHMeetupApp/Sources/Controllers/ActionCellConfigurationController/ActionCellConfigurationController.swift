a8e0d2b15305cd03a22e6b03fda0c39ce11de12a
Egor Petrov <petrov.bz.molod@gmail.com>
2017-03-29 21:49:31 +0300

    [C0VVJEIj] Changed type on PermissionsManager.Type

--- CHMeetupApp/Sources/Controllers/ActionCellConfigurationController/ActionCellConfigurationController.swift
+++ CHMeetupApp/Sources/Controllers/ActionCellConfigurationController/ActionCellConfigurationController.swift
@@ -10,61 +10,48 @@ import UIKit
 
 class ActionCellConfigurationController {
 
-  enum `Type` {
-    case reminders
-    case calendar
-  }
-
   typealias Action = () -> Void
 
-  func checkAccess(on viewController: UIViewController, for type: Type, with action: Action?) -> ActionPlainObject? {
+  func checkAccess(on viewController: UIViewController,
+                   for type: PermissionsManager.`Type`, with action: Action?) -> ActionPlainObject? {
     var actionPlainObject: ActionPlainObject?
 
     switch  type {
     case .reminders:
-      if !PermissionsManager.isAllowed(type: .reminders) {
+      if !PermissionsManager.isAllowed(type: type) {
         actionPlainObject = ActionPlainObject(text: "Включите оповещения, чтобы не пропустить событие".localized,
                                               imageName: "img_icon_notification", action: {
-                                              self.requireAccess(from: viewController, to: .reminders,
+                                              self.requireAccess(from: viewController, to: type,
                                                                  with: action)
         })
         return actionPlainObject
       }
     case .calendar:
       // FIXME: insert the real image
-      if !PermissionsManager.isAllowed(type: .calendar) {
+      if !PermissionsManager.isAllowed(type: type) {
         actionPlainObject = ActionPlainObject(text: "Включите календарь, чтобы не пропустить событие".localized,
                                               imageName: "img_icon_notification", action: {
-                                                self.requireAccess(from: viewController, to: .calendar,
+                                                self.requireAccess(from: viewController, to: type,
                                                                    with: action)
         })
         return actionPlainObject
       }
+    case .camera, .photosLibrary, .notifications:
+      break
     }
     return nil
   }
 
-  private func requireAccess(from viewController: UIViewController, to type: Type, with action: Action?) {
-    switch type {
-    case .reminders:
+  private func requireAccess(from viewController: UIViewController,
+                             to type: PermissionsManager.`Type`, with action: Action?) {
       PermissionsManager.requireAccess(from: viewController,
-                                       to: .reminders, completion: { success in
+                                       to: type, completion: { success in
                                         if success {
                                           DispatchQueue.main.async {
                                             action?()
                                           }
                                         }
       })
-    case .calendar:
-      PermissionsManager.requireAccess(from: viewController,
-                                       to: .calendar, completion: { success in
-                                        if success {
-                                          DispatchQueue.main.async {
-                                            action?()
-                                          }
-                                        }
-      })
-    }
   }
 
   func modelForActionCell(with actionPlainObject: ActionPlainObject) -> ActionTableViewCellModel {
