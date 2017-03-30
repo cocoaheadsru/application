1e3863ce87be811f2f86b058ce78f01309165905
Maxim Globak <maxim.globak@gmail.com>
2017-03-30 23:34:54 +0300

    [PTTZhOJK] Added cell and xib

---
+++ CHMeetupApp/Sources/ViewControllers/Events/Speech/subviews/SpeakerTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  SpeakerTableViewCell.swift
+//  CHMeetupApp
+//
+//  Created by Maxim Globak on 30.03.17.
+//  Copyright © 2017 CocoaHeads Community. All rights reserved.
+//
+
+import UIKit
+
+class SpeakerTableViewCell: PlateTableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
