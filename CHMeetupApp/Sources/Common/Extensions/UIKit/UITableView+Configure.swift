aca6011d4c64b30e28f225dcc84b1ecaaefbf628
Michael Galperin <m.galp509@gmail.com>
2017-03-31 08:39:09 +0300

    [dN0p8pqc] Removed: leftInset, rightInset

--- CHMeetupApp/Sources/Common/Extensions/UIKit/UITableView+Configure.swift
+++ CHMeetupApp/Sources/Common/Extensions/UIKit/UITableView+Configure.swift
@@ -13,9 +13,7 @@ private var associationKey = "tableView_bottom_inset"
 
 struct TableViewConfiguration {
   var topInset: CGFloat?
-  var leftInset: CGFloat?
   var bottomInset: CGFloat?
-  var rightInset: CGFloat?
   var estimatedRowHeight: CGFloat?
   var rowHeight: CGFloat?
   var backgroundColor: UIColor?
@@ -24,17 +22,13 @@ struct TableViewConfiguration {
   /// Without it when initializing you have to pass all existing parameters for correct type matching
   init(
     topInset: CGFloat? = nil,
-    leftInset: CGFloat? = nil,
     bottomInset: CGFloat? = nil,
-    rightInset: CGFloat? = nil,
     estimatedRowHeight: CGFloat? = nil,
     rowHeight: CGFloat? = nil,
     backgroundColor: UIColor? = nil
   ) {
     self.topInset = topInset
-    self.leftInset = leftInset
     self.bottomInset = bottomInset
-    self.rightInset = rightInset
     self.estimatedRowHeight = estimatedRowHeight
     self.rowHeight = rowHeight
     self.backgroundColor = backgroundColor
@@ -46,9 +40,7 @@ struct TableViewConfiguration {
 private var defaultConfiguration: TableViewConfiguration {
   return TableViewConfiguration(
     topInset: 8,
-    leftInset: 0,
     bottomInset: 8,
-    rightInset: 0,
     estimatedRowHeight: 100,
     rowHeight: UITableViewAutomaticDimension,
     backgroundColor: .clear
@@ -84,15 +76,9 @@ extension UITableView {
     if let topInset = configuration.topInset {
       self.contentInset.top = topInset
     }
-    if let leftInset = configuration.leftInset {
-      self.contentInset.left = leftInset
-    }
     if let bottomInset = configuration.bottomInset {
       self.contentInset.bottom = bottomInset
       defaultBottomInset = bottomInset
-    }
-    if let rightInset = configuration.rightInset {
-      self.contentInset.right = rightInset
     }
     if let estimated = configuration.estimatedRowHeight {
       self.estimatedRowHeight = estimated
