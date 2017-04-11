8ba3abcea48091187b1e916afbcec86eedc91cbb
Sam Mejlumyan <s.medzhlumyan@rambler-co.ru>
2017-04-11 08:27:53 +0300

    [OIHSNeVU] Added global displaycollectiondelegate

---
+++ CHMeetupApp/Sources/Common/Helpers/DisplayCollectionDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  DisplayCollectionDelegate.swift
+//  CHMeetupApp
+//
+//  Created by Sam Mejlumyan on 11/04/2017.
+//  Copyright © 2017 CocoaHeads Community. All rights reserved.
+//
+
+import UIKit
+
+protocol DisplayCollectionDelegate: class {
+  func updateUI()
+  func presentModalViewController(_ viewController: UIViewController)
+}
+
+extension UIViewController: DisplayCollectionDelegate {
+  func presentModalViewController(_ viewController: UIViewController) {
+    present(viewController, animated: true, completion: nil)
+  }
+
+  func updateUI() {
+    if let tableView = self.value(forKey: "tableView") as? UITableView {
+      tableView.reloadData()
+    }
+  }
+}
