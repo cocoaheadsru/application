c6860333016420953c6978ac870513ea9b448ada
DmitriyLis <expertgames@yandex.ru>
2017-04-22 19:25:21 +0300

    [Cyb5KMB4] Improved after comments

---
+++ CHMeetupApp/Sources/Common/Extensions/UIKit/UIImageView+RoundBorder.swift
@@ -0,0 +1,23 @@
+//
+//  UIImageView+RoundBorder.swift
+//  CHMeetupApp
+//
+//  Created by Dmitriy Lis on 22/04/2017.
+//  Copyright © 2017 CocoaHeads Community. All rights reserved.
+//
+
+import UIKit.UIImageView
+
+extension UIImageView {
+  func withWhiteRoundBorder(_ borderWidth: CGFloat) {
+    roundCorners()
+
+    let border = CAShapeLayer()
+    border.strokeColor = UIColor.white.cgColor
+    border.fillColor = UIColor.clear.cgColor
+    border.frame = bounds
+    border.lineWidth = (borderWidth * 2).round(0.5)
+    border.path = UIBezierPath(ovalIn: border.bounds).cgPath
+    layer.addSublayer(border)
+  }
+}
--- CHMeetupApp/Sources/Common/Views/ChooseProfilePhotoTableViewCell.swift
+++ CHMeetupApp/Sources/Common/Views/ChooseProfilePhotoTableViewCell.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 protocol ChooseProfilePhotoTableViewCellDelegate: class {
-  func chooseProfilePhotoCell(_ cell: ChooseProfilePhotoTableViewCell)
+  func chooseProfilePhotoCelldidPressOnPhoto(_ cell: ChooseProfilePhotoTableViewCell)
 }
 
 class ChooseProfilePhotoTableViewCell: UITableViewCell {
@@ -17,47 +17,23 @@ class ChooseProfilePhotoTableViewCell: UITableViewCell {
   @IBOutlet var photoImageView: UIImageView!
   @IBOutlet var addImageView: UIImageView!
 
-  private let photoBorder: CAShapeLayer = {
-    let border = CAShapeLayer()
-    border.strokeColor = UIColor.white.cgColor
-    border.fillColor = UIColor.clear.cgColor
-    return border
-  }()
-
-  private let addBorder: CAShapeLayer = {
-    let border = CAShapeLayer()
-    border.strokeColor = UIColor.white.cgColor
-    border.fillColor = UIColor.clear.cgColor
-    return border
-  }()
-
   weak var delegate: ChooseProfilePhotoTableViewCellDelegate?
 
   @IBAction func chooseButtonAction(_ sender: Any) {
-     delegate?.chooseProfilePhotoCell(self)
+     delegate?.chooseProfilePhotoCelldidPressOnPhoto(self)
   }
 
   override func layoutSubviews() {
     super.layoutSubviews()
 
-    photoImageView.roundCorners()
-    addImageView.roundCorners()
-
-    let lineWidth = (photoImageView.bounds.height * 0.08).round(0.5) // borderWidth = 4% of view height
-    photoBorder.frame = photoImageView.bounds
-    photoBorder.lineWidth = lineWidth
-    photoBorder.path = UIBezierPath(ovalIn: photoBorder.bounds).cgPath
-
-    addBorder.frame = addImageView.bounds
-    addBorder.lineWidth = lineWidth
-    addBorder.path = UIBezierPath(ovalIn: addBorder.bounds).cgPath
+    let borderWidth = photoImageView.bounds.height * 0.04
+    addImageView.withWhiteRoundBorder(borderWidth)
+    photoImageView.withWhiteRoundBorder(borderWidth)
   }
 
   override func awakeFromNib() {
     super.awakeFromNib()
 
-    photoImageView.layer.addSublayer(photoBorder)
-    addImageView.layer.addSublayer(addBorder)
     contentView.backgroundColor = UIColor(.lightGray)
     selectionStyle = .none
   }
