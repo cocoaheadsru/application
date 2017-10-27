f787d01d206ad30f77e1465b0f1232beea221b19
Makar Stetsenko <makazone@gmail.com>
2017-10-27 14:04:12 +0300

    [Give Speech] Fix #302]

--- CHMeetupApp/Sources/Controllers/GiveSpeechController/GiveSpeechController.swift
+++ CHMeetupApp/Sources/Controllers/GiveSpeechController/GiveSpeechController.swift
@@ -14,6 +14,7 @@ class GiveSpeechController {
     guard let userId = currentUser?.remoteId,
           let token = currentUser?.token
       else {
+        completion(false)
         return
     }
 
