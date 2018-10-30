215a951deb4e78043b2d1fc46a40724acfecf3ce
Roman Busygin <nskboy@gmail.com>
2018-10-30 14:19:15 +0300

    Improve bootstrap script readability

--- FirstSetup/firstSetup.sh
+++ FirstSetup/firstSetup.sh
@@ -5,14 +5,16 @@ echo Script was run...
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 echo HomeBrew:
 echo HomeBrew checking running...
+
 which -s brew
 if [[ $? != 0 ]] ; then
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-echo brew installed
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  echo brew installed
 else
-brew update
-echo brew updated
+  brew update
+  echo brew updated
 fi
+
 # -- 2 -- Installing swiftLint
 brew install swiftLint
 # -- 3 -- Installing natalie
@@ -26,25 +28,28 @@ cd
 cd $DIR
 
 moveHook() {
-cp prepare-commit-msg ../.git/hooks/
-if [[ $? != 0 ]] ; then
-echo !!!! We cant find .git/hooks/ folder, contact github.com/kirillzzy please
-else
-echo Hook moved
-fi
+  cp prepare-commit-msg ../.git/hooks/
+  if [[ $? != 0 ]] ; then
+    echo !!!! We can not find .git/hooks/ folder, contact github.com/kirillzzy please
+  else
+    echo Hook moved
+  fi
 }
 
 if [ ! -d "../.git/hooks" ]; then
-mkdir ../.git/hooks
+  mkdir ../.git/hooks
 fi
 moveHook
+
 # -- 6 -- carthage update
 echo Carthage:
 cd ../
+
 if [ ! -f "Cartfile" ]; then
   touch Cartfile
   echo Cartfile created
 fi
+
 carthage bootstrap --platform iOS
 echo carthage updated
 echo "Now you should write necessary libs in Cartfile and run command 'carthage update --platform iOS'"
